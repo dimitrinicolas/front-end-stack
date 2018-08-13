eae64b1478c5840e6ea250d3d2d3a7ab66481a13
Dimitri Nicolas <dimitri.nicolas.cn@gmail.com>
2018-08-13 14:07:41 +0200

    Add postcss-import-ext-glob dependency

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
-    require('postcss-easy-import'),
+    require('postcss-import-ext-glob'),
+    require('postcss-import'),
     require('postcss-nested'),
     require('postcss-extend-rule'),
     require('postcss-inline-media'),
