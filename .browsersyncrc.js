8fd98f2011fa8559caaa4a9297a3d9d88494d3e7
dimitrinicolas <dimitri.nicolas.cn@gmail.com>
2019-01-02 21:00:02 +0100

    Change build config organization

--- .browsersyncrc.js
+++ .browsersyncrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  proxy: 'localhost:3000',
+  proxy: 'localhost: 3000',
   port: 3001,
   browser: 'google chrome',
   files: [
-    'public/**/*.*'
+    'public /**/*.*'
   ],
   logLevel: 'info',
   logPrefix: 'Browser Sync',
--- .webpackrc.js
+++ .webpackrc.js
@@ -9,11 +9,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            cacheDirectory: true
-          }
+          loader: 'babel-loader'
         }
       }
     ]
